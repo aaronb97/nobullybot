8a0fc83850cb56ff1c70a0e7afaa29cf6b6e13b3
aaronb97 <aaron9becker7@gmail.com>
2021-10-16 14:33:43 -0400

    formatting

--- callbacks/messageCallback.ts
+++ callbacks/messageCallback.ts
@@ -4,27 +4,27 @@ function getRandomInt(max: number) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-const messageCallbackGenerator = (client: Client) => (
-  message: Message,
-): void => {
-  if (
-    message.mentions.has(client.user) &&
-    !message.content.includes("everyone")
-  ) {
-    const messages = [
-      "Bullybot is coolybot",
-      "You cannot bully the no bully bot",
-    ];
-    message.channel.send(messages[getRandomInt(messages.length)]);
-  }
+const messageCallbackGenerator =
+  (client: Client) =>
+  (message: Message): void => {
+    if (
+      message.mentions.has(client.user) &&
+      !message.content.includes("everyone")
+    ) {
+      const messages = [
+        "Bullybot is coolybot",
+        "You cannot bully the no bully bot",
+      ];
+      message.channel.send(messages[getRandomInt(messages.length)]);
+    }
 
-  if (message.content === "!ping") {
-    message.channel.send("Pong.");
-  }
+    if (message.content === "!ping") {
+      message.channel.send("Pong.");
+    }
 
-  if (message.content.includes("/rate")) {
-    message.channel.send("Wrong bot");
-  }
-};
+    if (message.content.includes("/rate")) {
+      message.channel.send("Wrong bot");
+    }
+  };
 
 export default messageCallbackGenerator;
